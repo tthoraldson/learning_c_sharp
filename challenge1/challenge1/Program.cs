4ad7a61505299364c31d3bc9ae6d0f143c762884
Theresa Thoraldson <tthoraldson@questarai.com>
2018-09-04 16:28:03 -0500

    challenge1 complete

---
+++ challenge1/challenge1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + name + ", you are " + age + " years old and you were born in " + month + ".");
+        }
+    }
+}
