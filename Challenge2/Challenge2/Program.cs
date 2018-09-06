40ae87e946966915704d562f505661f432ce9073
Theresa Thoraldson <tthoraldson@questarai.com>
2018-09-06 10:39:17 -0500

    challenge2 added

---
+++ Challenge2/Challenge2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Challenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your password");
+            var password = Console.ReadLine();
+
+            if (password == "secret")
+            {
+                Console.WriteLine("You have been authenticated.");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password.");
+            }
+        }
+    }
+}
