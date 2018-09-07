9e937569be19cb2a7db6150120b17ab934d6c309
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2018-09-07 18:19:16 -0500

    start of challenge3

---
+++ Challenge3/Challenge3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Challenge2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for (int loopCount = 0; loopCount < 5; loopCount++)
+            {
+                for (int displayNumber = 1; displayNumber < 11; displayNumber++)
+                {
+                    Console.WriteLine(displayNumber);
+                }
+                for (int displayNumber = 10; displayNumber > 0; --displayNumber)
+                {
+                    Console.WriteLine(displayNumber);
+                }
+
+            }
+        }
+    }
+}
